85936490fa667f8ec06e0d952857dbbb46b5badc
Luis Diaz Venero <luisdiazvenero@gmail.com>
2025-02-25 09:05:01 -0500

    ajuste de tabla clasificacion

--- src/components/dashboard/MapaProductos/columnsClasificacion.jsx
+++ src/components/dashboard/MapaProductos/columnsClasificacion.jsx
@@ -1,9 +1,22 @@
 // Colores para el Tipo de producto
 const typeColors = {
-    "Operaciones": "bg-red-100 text-red-600",
-    "Clientes/Venta": "bg-blue-100 text-blue-600",
+    "Operaciones": "bg-red-10 text-red-700",
+    "Clientes/Venta": "bg-blue-10 text-blue-700",
   };
   
+  // Colores para cada categoría
+const categoryColors = {
+    "Cereales, Granos y Carbohidratos": "bg-red-100 text-red-600",
+    "Frutas, verduras y Tubérculos": "bg-yellow-100 text-yellow-600",
+    "Proteínas": "bg-orange-100 text-orange-600",
+    "Lácteos y derivados": "bg-green-100 text-green-600",
+    "Embutidos": "bg-cyan-100 text-cyan-600",
+    "Grasas y aceites": "bg-blue-100 text-blue-600",
+    "Dulces y Snack": "bg-purple-100 text-purple-600",
+    "Bebidas": "bg-pink-100 text-pink-600",
+    "Salsas, aderezos y condimentos": "bg-red-100 text-red-500",
+    "Desechables Food Service": "bg-gray-200 text-gray-500",
+  };
   
   // Clasificaciones posibles
   const classifications = {
@@ -36,7 +49,7 @@ const typeColors = {
             />
           </a>
         ),
-        size: 0,
+        size: 100,
       },
     {
         accessorKey: "producto",
@@ -49,11 +62,36 @@ const typeColors = {
         size: 150,
       },
       {
-        accessorKey: "clasificacion",
-        header: "Clasificación",
+        accessorKey: "categoria",
+        header: "Categoría",
+        cell: ({ row }) => {
+            const colorClass = categoryColors[row.original.categoria] || "bg-gray-100 text-gray-600";
+            return (
+              <span className={`font-bold px-2 py-1 rounded-md ${colorClass}`}>
+                {row.original.categoria}
+              </span>
+            );
+          },
+
+        
+        size: 280,
+      },
+      {
+        accessorKey: "subcategoria",
+        header: "Sub Categoría",
         cell: ({ row }) => (
-          <span className="text-gray-700 font-medium">
-            {row.original.clasificacion}
+          <span className="text-gray-900 text-xs ">
+            {row.original.subcategoria}
+          </span>
+        ),
+        size: 150,
+      },
+      {
+        accessorKey: "lineaoperacion",
+        header: "Linea de Operación",
+        cell: ({ row }) => (
+          <span className="text-gray-700 text-xs font-medium">
+            {row.original.lineaoperacion}
           </span>
         ),
         size: 150,
@@ -65,18 +103,38 @@ const typeColors = {
         const tipo = row.original.tipo;
         const colorClass = typeColors[tipo] || "bg-gray-100 text-gray-600";
         return (
-          <span className={`font-bold px-2 py-1 rounded-md ${colorClass}`}>
+          <span className={`font-medium px-2 py-1 text-xs rounded-md ${colorClass}`}>
             {tipo}
           </span>
         );
       },
       size: 100,
     },
+    {
+        accessorKey: "clasificacioncliente",
+        header: "Cliente",
+        cell: ({ row }) => (
+            <span className="text-gray-900 text-xs">
+              {row.original.clasificacioncliente}
+            </span>
+          ),
+          size: 150,
+        },
+        {
+            accessorKey: "marca",
+            header: "Marca",
+            cell: ({ row }) => (
+                <span className="text-gray-900 ">
+                  {row.original.marca}
+                </span>
+              ),
+              size: 100,
+            },
     
     
     {
       accessorKey: "disponible",
-      header: "Disponible en",
+      header: "Pais",
       cell: ({ row }) => (
         <div className="flex gap-1">
           {row.original.disponible.map((pais, index) => (
@@ -84,7 +142,7 @@ const typeColors = {
           ))}
         </div>
       ),
-      size: 250,
+      size: 100,
     },
   ];
   
